b7fccf06553a6f65f87cc61344d823a599a842a8
Pranay.p <collegegoerp55@gmail.com>
2026-08-13 15:09:42 +0530

    Used if-else statements and string function startswith().

--- valid.py
+++ valid.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def validDigit(self, n, x):
+        num = str(n)
+        if str(x) in num:
+            if num.startswith(str(x)):
+                return False 
+            else:
+                return True
+        else:
+            return False 
+        
