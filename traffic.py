507fb2bc6b845ecdd821af354543ed313fb9c3c0
Pranay.p <collegegoerp55@gmail.com>
2026-08-13 15:07:33 +0530

    Used simple if-else statements.

--- traffic.py
+++ traffic.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def trafficSignal(self, timer):
+        if timer == 0:
+            return "Green"
+        elif timer == 30:
+            return "Orange"
+        elif 30 < timer <= 90:
+            return "Red"
+        else:
+            return "Invalid"
 
+
+        
