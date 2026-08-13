f8e7977669e16c23048cc44316791245803b201b
Pranay.p <collegegoerp55@gmail.com>
2026-08-13 22:42:41 +0530

    Using iterative function looping and string functions.

--- sumNmultiply.py
+++ sumNmultiply.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def sumAndMultiply(self, n):
+        num = list(str(n))
+        original = []
+        for digit in num:
+            if digit != str(0):
+                original.append(int(digit))
+            else:
+                continue      
+        if not original:
+            return 0
+        value = sum(original)
+        final = int("".join(map(str,original)))
+        ans = int(value) * final
+        return ans
 
+
+        
