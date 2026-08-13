d9ee88511b72a6da6c6de9266df31b1a02d0e385
Pranay.p <collegegoerp55@gmail.com>
2026-08-13 15:11:36 +0530

    Used iterative for loops over lists sorting original and unique elements for reference.

--- digitfreq.py
+++ digitfreq.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def digitFrequencyScore(self, n):
+        num = list(str(n))
+        hh = list(set(num))
+        arr = []
+        value = 0
+        for digit in hh:
+           value = num.count(digit) * int(digit)
+           arr.append(value)
+           
+        total = sum(arr)
+        return total 
 
+
+
+
+       
+        
