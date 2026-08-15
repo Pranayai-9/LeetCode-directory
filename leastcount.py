fff6b185d5a1e3777e8b58cccaa881a55512b1ad
Pranay.p <collegegoerp55@gmail.com>
2026-08-15 20:49:58 +0530

    Using count function iteratively and using the indices of minimum tied frequencies to obtain frequency of least digit.

--- leastcount.py
+++ leastcount.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def getLeastFrequentDigit(self, n):
+        num = list(str(n))
+        frequency = []
+        
+        for digit in num:
+            value = num.count(digit)
+            frequency.append(value)
+            
+        ans = min(frequency)
+        
+        
+        indices = [index for index, value in enumerate(frequency) if value == ans]
+        tied_digits = [int(num[idx]) for idx in indices]
+        
+        return min(tied_digits) 
 
