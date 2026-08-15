6ad6133a4bd0e64ee360011a08e5604047a8ca4c
Pranay.p <collegegoerp55@gmail.com>
2026-08-15 20:21:11 +0530

    Using the iterative computing of place-digit value of the given string converted from integer.

--- decimal.py
+++ decimal.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def decimalRepresentation(self,n):
+        s=str(n)
+        ans=[]
+        length=len(s)
 
+        for i in range(length):
+            digit=int(s[i])
+
+            if digit!=0:
+                ans.append(digit*(10**(length-i-1)))
+
+        return ans
