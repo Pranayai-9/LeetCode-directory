2a3e8bb7600b8300a2765693b2d178716b50c5af
Pranay.p <collegegoerp55@gmail.com>
2026-08-15 20:05:27 +0530

    Useing brute force method and replace function.

--- remove0.py
+++ remove0.py
@@ -1,1 +1,18 @@
+#Iterative looping and removing "0"
+class Solution(object):
+    def removeZeros(self, n):
+        num = list(str(n))
+        if "0" not in num:
+            return n
+        else:
+            while "0" in num:
+                num.remove("0")
+        result = "".join(num)
+        return int(result)
 
+        
+#Replace function:
+def removeZeros(self, n):
+        cleaned_str = str(n).replace("0", "")
+    
+        return int(cleaned_str) if cleaned_str else 0
