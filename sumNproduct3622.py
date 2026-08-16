bb75a25ccb0060b678bfc54de9255ce0f64a618a
Pranay.p <collegegoerp55@gmail.com>
2026-08-16 10:52:32 +0530

    Iterative looping and variable assignment.

--- sumNproduct3622.py
+++ sumNproduct3622.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def checkDivisibility(self, n):
+        num = str(n)
+        value = 1
+        for digit in num:
+            value *= int(digit)
+        ans = sum(int(digit) for digit in num) + value 
+        if n % int(ans) == 0:
+            return True
+        else:
+            return False 
 
+        
+        
