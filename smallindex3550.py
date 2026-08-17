c419709c67ba656492327569d0c61076918224de
Pranay.p <collegegoerp55@gmail.com>
2026-08-17 11:07:58 +0530

    Two approaches: Brute force with string functions and clean while loop.

--- smallindex3550.py
+++ smallindex3550.py
@@ -1,1 +1,34 @@
+class Solution(object):
+    def smallestIndex(self, nums):
+        i = 0  
+        while i < len(nums):
+           digitList = []
+           pop = list(str(nums[i]))
+           for digit in pop:
+            digitList.append(int(digit)) 
+            if sum(digitList) == i:
+                return i  
+            else:
+                i = i + 1  
+                
+       
+        return -1
+
+#O(N) & O(1) complexity:
+class Solution(object):
+    def smallestIndex(self, nums):
+        # A single, clean for loop tracking index and value
+        for i, num in enumerate(nums):
+            digit_sum = 0
+            
+            # Extract digits mathematically without strings or lists
+            while num > 0:
+                digit_sum += num % 10  # Gets the last digit
+                num //= 10             # Removes the last digit
+            
+            # Check the condition immediately
+            if digit_sum == i:
+                return i
+                
+        return -1
 
