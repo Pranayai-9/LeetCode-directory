a9eaa79ba7410019bc154321d900003590a35b1a
Pranay.p <collegegoerp55@gmail.com>
2026-08-17 19:16:41 +0530

    Using nested loops over the numeric arrat for exploring all possible products.

--- Maxp3536.py
+++ Maxp3536.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def maxProduct(self, n):
+        num = list(str(n))
+        x = [int(x) for x in num]
+        product = []
+        
+        for i in range(len(num)):
+            for j in range(i+1,len(num)):
+                value = int(num[i]) * int(num[j])
+                product.append(value) 
+                
+        result  = max(product)
+        return result 
 
+
+        
