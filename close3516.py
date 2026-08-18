d40362db148a1dd44ddfd3c70c405badc2b81d86
Pranay.p <collegegoerp55@gmail.com>
2026-08-18 11:53:18 +0530

    Finding the minimum absolute distance between first two numbers.

--- close3516.py
+++ close3516.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def findClosest(self, x, y, z):
+        target = z
+        val1 = abs(target - x)
+        val2 = abs(target - y)
+        if val1 < val2:
+            return 1
+        elif val1 > val2:
+            return 2
+        else:
+            return 0
+        
