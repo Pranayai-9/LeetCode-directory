c876d43abbe840432596168215187c870770ea69
Pranay.p <collegegoerp55@gmail.com>
2026-08-18 13:22:23 +0530

    Optimized version by making Maxweight/w as the limit. O(1) and O(1) complexities.

--- Maxcon3492.py
+++ Maxcon3492.py
@@ -11,7 +11,26 @@ class Solution(object):
             con += 1
         return con
 
+
+#Optimized code maxweight/w as constraint:
+class Solution(object):
+    def maxContainers(self, n, w, maxWeight):
+        weight = 0
+        deck = pow(n,2)
+        con = 0
+        limit = maxWeight/w
+        if limit >= deck:
+            return deck
+        else:
+            return limit 
+    
+
             
+
+
+
+        
+               
 
 
 
