ff6fcbef1a24c0de339f325effffd67885ae6f82
Pranay.p <collegegoerp55@gmail.com>
2026-08-18 19:56:22 +0530

    Cleaning the string by removing the punctuations and spaces using lower function and reversing the string by string slicing.

--- strings/Palindrome125.py
+++ strings/Palindrome125.py
@@ -1,1 +1,8 @@
-
+class Solution(object):
+    def isPalindrome(self, s):
+       clean = "".join(char.lower() for char in s if char.isalnum())
+       if clean == clean[::-1]:
+          return True
+       else:
+          return False
+        
