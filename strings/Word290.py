6a83add7bf8c339ac1b149a003aa2e6c5a1840fe
Pranay.p <collegegoerp55@gmail.com>
2026-08-18 19:53:50 +0530

    The set function returns the unique elements of the string and zip function matches the pattern and string.

--- strings/Word290.py
+++ strings/Word290.py
@@ -1,1 +1,7 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        original = s.split()
+        if len(pattern) != len(original):
+            return False
+        return len(set(pattern)) == len(set(original)) == len(set(zip(pattern, original)))
 
