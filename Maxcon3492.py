8db329cfec38f56f8fc28ee9374a3e0890eb855a
Pranay.p <collegegoerp55@gmail.com>
2026-08-18 12:58:52 +0530

    Using brute force method: Filling the cargo decks one by one until constraints are reached.

--- Maxcon3492.py
+++ Maxcon3492.py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def maxContainers(self, n, w, maxWeight):
+        weight = 0
+        decks = pow(n,2)
+        con = 0
+        while weight < maxWeight and decks != 0:
+            weight += w
+            if weight > maxWeight:
+                break
+            decks -= 1
+            con += 1
+        return con
 
+            
+
+
+
+        
+        
