ad0473f1155c7ef631048bb4dc64789ec266c8da
Pranay.p <collegegoerp55@gmail.com>
2026-08-18 12:18:21 +0530

    Brute force approach: looping through every array item with assigned operation, until array iteem is 0 as constraint.

--- Minop3512.py
+++ Minop3512.py
@@ -1,1 +1,17 @@
-
+class Solution(object):
+    def minOperations(self, nums, k):
+        op = 0
+        if sum(nums) % k == 0:
+            return 0
+        else:
+            for i in range(len(nums)):
+                while nums[i] != 0:
+                    nums[i] -=  1
+                    op += 1
+                    if sum(nums) % k == 0:
+                        return op
+                    else:
+                        continue
+            return op
+       
+        
