585abdf05904e6db67d2c438526746d539862728
Pranay.p <collegegoerp55@gmail.com>
2026-08-19 12:13:07 +0530

    Using bitwise xor operation.

--- Array/single.py
+++ Array/single.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        for item in nums:
+            if nums.count(item) == 1:
+                return item
+            else:
+                continue
+        
+
+#Bitwise operation
+class Solution(object):
+    def singleNumber(self, nums):
+        result = 0
+        for item in nums:
+            result ^= item  # XOR operation
+        return result
 
