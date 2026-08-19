b95f72d1f473e9fbad33dea9b9868feef26df57f
Pranay.p <collegegoerp55@gmail.com>
2026-08-19 12:23:41 +0530

    Early-Exit Optimization (Saves Memory)

--- Array/Single136.py
+++ Array/Single136.py
@@ -3,4 +3,22 @@ class Solution(object):
         return len(nums) != len(set(nums))
             
 
+#2.
+class Solution(object):
+    def containsDuplicate(self, nums):
+        if len(nums) - len(set(nums)) != 0:
+            return True
+        else:
+            return False
         
+
+3.
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True # Found duplicate early, stop immediately
+            seen.add(num)
+        return False
+
