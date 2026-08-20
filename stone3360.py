6d4218b6652fbe0b418f3f8bcf6216ceb9587e9b
Pranay.p <collegegoerp55@gmail.com>
2026-08-20 12:33:32 +0530

    Using simple if else statements.

--- stone3360.py
+++ stone3360.py
@@ -1,1 +1,15 @@
+class Solution:
+    def canAliceWin(self, n):
+        if 10 <= n <= 18:
+            return True
+        if 27 <= n <= 33:
+            return True
+        if 40 <= n <= 44:
+            return True
+        if 49 <= n <= 51:
+            return True
+        if n == 54:
+            return True
+            
+        return False
 
