0b0874c04bfb61bfbd39ea34959bd6c0dee4da67
Pranay.p <collegegoerp55@gmail.com>
2026-08-21 11:22:07 +0530

    Iterative looping and removing first occurence.

--- 383ransom.py
+++ 383ransom.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        for char in ransomNote:
+            if char in magazine:
+                # Remove only the first occurrence of the character
+                magazine = magazine.replace(char, "", 1)
+            else:
+                return False
+        return True
 
