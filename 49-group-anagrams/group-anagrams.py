960f50a3220ad60ed66262b1f590d1608495394e
Pranayai-9 <collegegoerp55@gmail.com>
2026-08-29 11:36:01 +0530

    Time: 23 ms (46.22%) | Memory: 15.9 MB (86.58%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """:type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = defaultdict(list)
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            ans[sorted_s].append(s)
+        return list(ans.values())
+
+        
